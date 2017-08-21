4a57357f016925dbe1287b278108a039b436cfd7
linh1987 <linh1987@gmail.com>
2017-08-21 11:34:37 +0700

    Initial Hero Tournament Project

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+export interface Hero {
+    id: number;
+    name: string;
+}
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'app';
+}
